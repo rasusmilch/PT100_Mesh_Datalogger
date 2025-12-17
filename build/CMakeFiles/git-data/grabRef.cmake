8f8bd2692332c22fc8c073abc15eac973443329e
Robert Susmilch <robert-github@susmilch.com>
2025-12-17 14:20:24 -0600

    Setup sdkconfig

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Coding_Projects/PT100_Mesh_Datalogger/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/esp/v5.5.1/esp-idf/.git")
+set(GIT_DIR "D:/Coding_Projects/PT100_Mesh_Datalogger/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
